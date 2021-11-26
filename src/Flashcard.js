78cbba72bf41b745989c99a4aba127f06f5e320c
liamdoan <lamdoan9898@gmail.com>
2021-11-26 09:37:30 +0200

    1st commit

---
+++ src/Flashcard.js
@@ -0,0 +1,55 @@
+// flashcard component
+
+import React, {useState, useEffect, useRef} from 'react'
+
+const Flashcard = ({flashcard}) => {
+
+    const [flip, setFlip] = useState(false)
+    const [height, setHeight] = useState('initial')
+
+    const frontEl = useRef()
+    const backEl = useRef()
+
+    function setMaxHeight() {
+        const frontHeight = frontEl.current.getBoundingClientRect().height
+        const backHeight = backEl.current.getBoundingClientRect().height
+
+        setHeight(Math.max(frontHeight, backHeight, 100))
+    }
+
+    useEffect(setMaxHeight, [flashcard.question, flashcard.answer, flashcard.options])
+    
+    useEffect(() => {
+        window.addEventListener('resize', setMaxHeight)
+        return () => window.removeEventListener('resize', setMaxHeight)
+    })
+
+    return (
+        <div
+            className = {`card ${flip ? 'flip' : ''}`}
+            // have a class of CARD for every flashcard
+            // and if FLIP is true it will have class FLIP, otherwise none
+            style={{height: height}}
+            onClick = {() => setFlip(!flip)}>
+
+            <div className="front" ref={frontEl}>
+                {flashcard.question}          
+                <div className="flashcard-options">
+                    {flashcard.options.map(option => {
+                        return <div className="flashcard-option">{option}</div>
+                    })}
+                </div>
+            </div>
+
+            <div className="back" ref={backEl}>
+                {flashcard.answer}
+            </div>
+
+            {/* {flip ? flashcard.answer : flashcard.question} */}
+        </div>
+    )
+}
+
+export default Flashcard
+
+// 18.51 to flip
